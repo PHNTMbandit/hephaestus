6024c685d7836e79637933a764c400d17213f2fc
Domenic Pittari <dom.pittari@gmail.com>
2026-08-07 17:20:44 +0900

    fix(auth-client.ts): remove base url from auth client
    interferes with dynamic base url setup in better auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,5 @@ import { usernameClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
 export const { useSession, signIn, signOut, signUp, getSession } = createAuthClient({
-  baseURL: 'http://localhost:3000',
   plugins: [usernameClient()],
 })
